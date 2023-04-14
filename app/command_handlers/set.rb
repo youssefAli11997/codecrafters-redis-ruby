3459d5019696afff45ec48df56be69285c9e0ed7
youssefAli11997 <youssefali6212@gmail.com>
2023-04-14 21:22:23 +0200

    set with expiration

--- app/command_handlers/set.rb
+++ app/command_handlers/set.rb
@@ -8,13 +8,36 @@ module CommandHandlers
         def self.handle(client, command_array)
             case command_array.length
             when 3
-                if DataStore.set(command_array[1], command_array[2]) == true
-                    client.puts RespEncoder.encode('+OK')
+                handle_normal_set(client, command_array[1], command_array[2])
+            when 5
+                if command_array[3].upcase == 'PX'
+                    handle_expiry_set(client, command_array[1], command_array[2], command_array[4])
                 else
-                    client.puts RespEncoder.encode('$-1')
+                    client.puts RespEncoder.encode("-syntax error")
                 end
             else
                 client.puts RespEncoder.encode("-wrong number of arguments for 'set' command")
+            end
+        end
+
+        private
+
+        def self.handle_normal_set(client, key, value)
+            if DataStore.set(key, value) == true
+                client.puts RespEncoder.encode('+OK')
+            else
+                client.puts RespEncoder.encode('$-1')
+            end
+        end
+
+        def self.handle_expiry_set(client, key, value, expiry_after_milliseconds)
+            expiry_after_milliseconds = expiry_after_milliseconds.to_i
+            if expiry_after_milliseconds < 0
+                client.puts RespEncoder.encode('-expiration time cannot be negative')
+            elsif DataStore.set(key, value, expiry_after_milliseconds: expiry_after_milliseconds) == true
+                client.puts RespEncoder.encode('+OK')
+            else
+                client.puts RespEncoder.encode('$-1')
             end
         end
     end
--- app/data_store.rb
+++ app/data_store.rb
@@ -1,12 +1,22 @@
 class DataStore
     HASH = {}
+    EXPIRY_AT = {}
 
-    def self.set(key, value)
+    def self.set(key, value, expiry_after_milliseconds: nil)
         HASH[key] = value
+        unless expiry_after_milliseconds.nil?
+            EXPIRY_AT[key] = Time.now + (expiry_after_milliseconds / 1000)
+        end
         true
     end
 
     def self.get(key)
-        HASH[key]
+        if EXPIRY_AT.key?(key) && EXPIRY_AT[key] < Time.now
+            EXPIRY_AT.delete(key)
+            HASH.delete(key)
+            nil
+        else
+            HASH[key]
+        end
     end
 end
