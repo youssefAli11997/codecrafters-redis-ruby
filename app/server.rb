77e89930f1ed17c08db64220d590f4f04c5a2061
youssefAli11997 <youssefali6212@gmail.com>
2023-04-14 15:41:48 +0200

    better parsing

--- app/server.rb
+++ app/server.rb
@@ -7,28 +7,73 @@ class YourRedisServer
 
   def start
     server = TCPServer.new(@port)
-
     loop do
-      Thread.start(server.accept) do |client|
-        request = client.gets
-        puts request
-        commands = request.split '\n'
-        puts commands
-        commands.each do |command|
-          client.puts handle_command(command)
-        end
-        client.close
-      end
+      client = server.accept
+      handle_client(client)
     end
   end
 
   private
-  
-  def handle_command(command)
-    if command == 'ping' || command == 'PING'
-      "+PONG\r"
+
+  def handle_client(client)
+    command_array = read_resp_array(client)
+    handle_command_array(client, command_array)
+  end
+
+  def read_resp_array(client)
+    array_length = client.gets[1..-3].to_i # "*1\r\n", ignoring * and \r\n
+    array = []
+
+    # p 'array_length'
+    # p array_length
+    
+    array_length.times do
+      item = parse_line(client)
+      # p 'parsed line'
+      # p item
+      array << item
     end
-    "- ERR unknown command"
+
+    array
+  end
+
+  def parse_line(client)
+    line = client.gets
+    type = line[0]
+    data = line[1..-3]
+
+    # p 'line'
+    # p line
+
+    case type
+    when '$'
+      # data is a bulk string length in this case
+      client.gets[0..-3] # read the bulk string itself from the next line
+    when '+'
+      # data is a simple string
+      data
+    when ':'
+      # data is an integer
+      data.to_i
+    when '*'
+      # data is an array
+      # I may handle it later if there is a need
+    end
+  end
+
+  def handle_command_array(client, command_array)
+    command = command_array.first.upcase
+
+    case command
+    when 'PING'
+      client.puts resp_encode('+PONG')
+    else
+      client.puts resp_encode("-ERR unknown command #{command}")
+    end
+  end
+
+  def resp_encode(message)
+    "#{message}\r\n"
   end
 end
 
